584d6269363988b2755162b0a047bc36d575b297
Daria Neznaeva <darianeznaeva@gmail.com>
2021-11-11 14:12:59 -0600

    first commit

---
+++ app.js
@@ -0,0 +1,5 @@
+const messageDB = {
+day : ['moon', 'sun', 'earth', 'wind', 'air'],
+output: ['a very good day', 'good day', 'ok day', 'bad day'],
+advice: ['The weather is beautiful, maybe you should go for a walk', 'Did you sleep enough, maybe you need to take a nap', 'It seems you are hungry, go eat a snack', 'When did you go running last time? It seems today is the perfect day to start']
+};
